170449a388040c628d6722bdd630a48cd758bbef
cc44h16a <cc4_4h16a@yahoo.com.hk>
2024-09-12 16:32:34 -0700

    Update imputers.py
    Updated type hint in imputers.py

--- imputers.py
+++ imputers.py
@@ -8,11 +8,11 @@ class MarketIDImputer(TransformerMixin):
     def __init__(self) -> None:
         self.store_to_market = {}
 
-    def fit(self, X, y=None):
+    def fit(self, X: pd.DataFrame, y: pd.Series | np.ndarray = None):
         self.store_to_market = X.sort_values(by='created_at')[['store_id', 'market_id']].dropna(subset='market_id').drop_duplicates(keep='last').set_index('store_id').to_dict()['market_id']
         return self
 
-    def transform(self, X, y=None):
+    def transform(self, X: pd.DataFrame, y: pd.Series | np.ndarray = None) -> pd.DataFrame:
         X = X.copy()
         X['market_id'] = X['store_id'].apply(lambda x: self.store_to_market[x] if x in self.store_to_market else None)
         return X
@@ -21,24 +21,24 @@ class PrimaryCategoryImputer(TransformerMixin):
     def __init__(self) -> None:
         self.store_to_primary = {}
 
-    def fit(self, X, y=None):
+    def fit(self, X: pd.DataFrame, y: pd.Series = None):
         # Some of the store has more than one primary category, choosing the latest one by sorting by `created_at`
         self.store_to_primary = X.sort_values(by='created_at')[['store_id', 'store_primary_category']].dropna(subset='store_primary_category').drop_duplicates(subset='store_id', keep='last').set_index('store_id')['store_primary_category'].to_dict()
         return self
 
-    def transform(self, X, y=None):
+    def transform(self, X: pd.DataFrame, y: pd.Series = None) -> pd.DataFrame:
         X = X.copy()
         X['store_primary_category'] = X['store_primary_category'].fillna(X['store_id'].apply(lambda x: self.store_to_primary[x] if x in self.store_to_primary else None))
         return X
     
 class MarketFeatureImputer(TransformerMixin):
-    def __init__(self, method='median', market_features=None) -> None:
+    def __init__(self, method: str = 'median', market_features: list[str] = None) -> None:
         self.method = method
         self.market_features = market_features
         self.nearest = {}
         self.market_features_median = {}
 
-    def fit(self, X, y=None):
+    def fit(self, X: pd.DataFrame, y: pd.Series | np.ndarray = None):
         # Some of the store has more than one primary category, choosing the latest one by sorting by `created_at`
         if self.method == 'median':
             for feature in self.market_features:
@@ -56,7 +56,7 @@ class MarketFeatureImputer(TransformerMixin):
             raise ValueError('No such method.')
         return self
 
-    def transform(self, X, y=None):
+    def transform(self, X: pd.DataFrame, y: pd.Series | np.ndarray = None) -> pd.DataFrame:
         X = X.copy()
         if self.method == 'median':
             for feature in self.market_features:
@@ -69,7 +69,7 @@ class MarketFeatureImputer(TransformerMixin):
         return X
 
 class Imputers():
-    def __init__(self, market_features):
+    def __init__(self, market_features: list[str]):
         self.market_features = market_features
         self.pipeline = Pipeline(
                                     [
